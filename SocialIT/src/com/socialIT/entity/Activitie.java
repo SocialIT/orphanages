29fe8fd44bf0a68a95850f39dab647eae8311e5e
xserjjx <terman_serjj@mail.ru>
2012-11-06 17:12:43 +0200

    Am adaugat niste entitati

---
+++ SocialIT/src/com/socialIT/entity/Activitie.java
@@ -0,0 +1,62 @@
+package com.socialIT.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "activities")
+public class Activitie {
+	private Integer id;
+	private String name;
+	private String description;
+	private String pictureLink;
+	private Integer orphanageId;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	@Column(name = "name")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name = "description")
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Column(name = "picture_link")
+	public String getPictureLink() {
+		return pictureLink;
+	}
+	public void setPictureLink(String pictureLink) {
+		this.pictureLink = pictureLink;
+	}
+	
+	@Column(name = "orphanage_id")
+	public Integer getOrphanageId() {
+		return orphanageId;
+	}
+	public void setOrphanageId(Integer orphanageId) {
+		this.orphanageId = orphanageId;
+	}
+	
+	
+}
